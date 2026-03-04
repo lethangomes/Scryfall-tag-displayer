378b737b6c45b9c0c56beb265f4f2753cf34e5af
Lucas Gomes <lethangomes@gmail.com>
2026-03-03 16:03:58 -0800

    cleaned up the async calls a bit

--- showTags.js
+++ showTags.js
@@ -109,9 +109,7 @@ function createTable(headers, content, cellBuilder){
 
 elements = document.getElementsByClassName("button-n")
 
-// Check if this has already run
-if(!document.getElementById('tag-container')){
-
+async function addTags(){
     // Query body we will send to scryfall tagger. No, I don't know what the query property is.
     let qBody = {"query":"\n        query FetchCard(\n          $set: String!\n          $number: String!\n          $back: Boolean = false\n          $moderatorView: Boolean = false\n        ) {\n          card: cardBySet(set: $set, number: $number, back: $back) {\n            ...CardAttrs\n            backside\n            flipsideDisplayName\n            hasAlternateName\n            layout\n            scryfallUrl\n            sideNames\n            twoSided\n            rotatedLayout\n            taggings(moderatorView: $moderatorView) {\n              ...TaggingAttrs\n              tag {\n                ...TagAttrs\n                ancestorTags {\n                  ...TagAttrs\n                }\n              }\n            }\n            relationships(moderatorView: $moderatorView) {\n              ...RelationshipAttrs\n            }\n          }\n        }\n        \n  fragment CardAttrs on Card {\n    artImageUrl\n    backside\n    cardImageUrl\n    collectorNumber\n    displayName\n    id\n    illustrationId\n    name\n    oracleId\n    printingId\n    set\n  }\n\n        \n  fragment RelationshipAttrs on Relationship {\n    classifier\n    classifierInverse\n    annotation\n    subjectId\n    subjectName\n    createdAt\n    creatorId\n    foreignKey\n    id\n    name\n    pendingRevisions\n    relatedId\n    relatedName\n    status\n    type\n  }\n\n        \n  fragment TagAttrs on Tag {\n    category\n    createdAt\n    creatorId\n    id\n    name\n    namespace\n    pendingRevisions\n    slug\n    status\n    type\n    hasExemplaryTagging\n    description\n  }\n\n        \n  fragment TaggingAttrs on Tagging {\n    annotation\n    subjectId\n    createdAt\n    creatorId\n    foreignKey\n    id\n    pendingRevisions\n    type\n    status\n    weight\n  }\n\n      ","variables":{"set":"jud","number":"140","back":false,"moderatorView":false},"operationName":"FetchCard"}
 
@@ -122,124 +120,126 @@ if(!document.getElementById('tag-container')){
             console.log(link)
 
             // Fetch scryfall tagger page
-            fetch(link).then((data) => {
-                data.text().then((text) => {
-                    // Find the csrf token
-                    let idx = text.indexOf('<meta name=\"csrf-token\" content=\"')
-                    idx += 33;
-                    let token = '';
-                    while(text[idx] != '\"'){
-                        token = token + text[idx];
-                        idx++;
-                    }
+            let data = await fetch(link)    
+            let text = await data.text()
 
-                    // Get set and card number from tagger link
-                    let linkpath = link.split('/')
-                    qBody.variables.number = linkpath[linkpath.length - 1]
-                    qBody.variables.set = linkpath[linkpath.length - 2]
-                    
-                    // Query scryfall tagger
-                    fetch('https://tagger.scryfall.com/graphql', 
-                        {
-                            method:'POST', 
-                            body: JSON.stringify(qBody),
-                            headers: {'X-CSRF-TOKEN': token, 'Content-Type':"application/json"}
-                        },).then((r) => {
-                            r.json().then((json) => {
-                                // Get information we care about
-                                console.log(json.data)
-                                let tags = json.data.card.taggings;
-                                let relationships = json.data.card.relationships;
-                                tags = tags.map((t) => ({
-                                    name: t.tag.name, 
-                                    type: t.tag.type, 
-                                    description: t.tag.description,
-                                    ancestors: t.tag.ancestorTags.map((atag) => ({name: atag.name, desc: atag.description}))
-                                }))
-                                console.log(tags)
-                                console.log(relationships)
+            // Find the csrf token
+            let idx = text.indexOf('<meta name=\"csrf-token\" content=\"')
+            idx += 33;
+            let token = '';
+            while(text[idx] != '\"'){
+                token = token + text[idx];
+                idx++;
+            }
 
-                                // Create div to hold tags
-                                let tagDiv = document.createElement('div');
+            // Get set and card number from tagger link
+            let linkpath = link.split('/')
+            qBody.variables.number = linkpath[linkpath.length - 1]
+            qBody.variables.set = linkpath[linkpath.length - 2]
+            
+            // Query scryfall tagger
+            let r = await fetch('https://tagger.scryfall.com/graphql', 
+                {
+                    method:'POST', 
+                    body: JSON.stringify(qBody),
+                    headers: {'X-CSRF-TOKEN': token, 'Content-Type':"application/json"}
+                },) 
+            let json = await r.json()
 
-                                let cardProfileElement; // The area with card info
-                                for(child of document.getElementById('main').childNodes){
-                                    if(child.className === 'card-profile'){
-                                        cardProfileElement = child;
-                                        break;
-                                    }
-                                }
+            // Get information we care about
+            console.log(json.data)
+            let tags = json.data.card.taggings;
+            let relationships = json.data.card.relationships;
+            tags = tags.map((t) => ({
+                name: t.tag.name, 
+                type: t.tag.type, 
+                description: t.tag.description,
+                ancestors: t.tag.ancestorTags.map((atag) => ({name: atag.name, desc: atag.description}))
+            }))
+            console.log(tags)
+            console.log(relationships)
 
-                                // Oracle tags table
-                                let otags = tags.filter((t) => t.type === 'ORACLE_CARD_TAG').map((t) => ({name: t.name, desc: t.description}))
-                                if(otags.length > 0){
-                                    let oTagTable = createTable(['Oracle Tags'], otags, buildTagTableCell('otag'))
-                                    tagDiv.appendChild(oTagTable)
-                                }
-                                
+            // Create div to hold tags
+            let tagDiv = document.createElement('div');
 
-                                // Art tags table
-                                let atags = tags.filter((t) => t.type === 'ILLUSTRATION_TAG').map((t) => ({name: t.name, desc: t.description}))
-                                if(atags.length > 0){
-                                    let aTagTable = createTable(['Art Tags'], atags, buildTagTableCell('atag'))
-                                    tagDiv.appendChild(aTagTable)
-                                }
+            let cardProfileElement; // The area with card info
+            for(child of document.getElementById('main').childNodes){
+                if(child.className === 'card-profile'){
+                    cardProfileElement = child;
+                    break;
+                }
+            }
 
-                                // Relationships
-                                if(relationships.length > 0){
-                                    let relTable = createTable(['Related Cards'], relationships, buildRelationshipCell(json.data.card.name))
-                                    tagDiv.appendChild(relTable);
-                                }
+            // Oracle tags table
+            let otags = tags.filter((t) => t.type === 'ORACLE_CARD_TAG').map((t) => ({name: t.name, desc: t.description}))
+            if(otags.length > 0){
+                let oTagTable = createTable(['Oracle Tags'], otags, buildTagTableCell('otag'))
+                tagDiv.appendChild(oTagTable)
+            }
+            
 
-                                let divider = document.createElement('div');
-                                divider.style.height = '0';
-                                divider.style.flexBasis = '100%';
-                                tagDiv.appendChild(divider);
+            // Art tags table
+            let atags = tags.filter((t) => t.type === 'ILLUSTRATION_TAG').map((t) => ({name: t.name, desc: t.description}))
+            if(atags.length > 0){
+                let aTagTable = createTable(['Art Tags'], atags, buildTagTableCell('atag'))
+                tagDiv.appendChild(aTagTable)
+            }
 
-                                // Inherited oracle tags table
-                                let inheritedOTags = new Map()
-                                for(let tag of tags.filter((t) => t.type === 'ORACLE_CARD_TAG')){
-                                    for(ancestor of tag.ancestors){
-                                        inheritedOTags.set(ancestor.name, ancestor);
-                                    }
-                                }
-                                if([...inheritedOTags.values()].length > 0){
-                                    let iOTagTable = createTable(['Inherited Oracle Tags'], [...inheritedOTags.values()], buildTagTableCell('otag'))
-                                    tagDiv.appendChild(iOTagTable)
-                                }
-                                
+            // Relationships
+            if(relationships.length > 0){
+                let relTable = createTable(['Related Cards'], relationships, buildRelationshipCell(json.data.card.name))
+                tagDiv.appendChild(relTable);
+            }
 
-                                // Inherited art tags table
-                                let inheritedATags = new Map()
-                                for(let tag of tags.filter((t) => t.type === 'ILLUSTRATION_TAG')){
-                                    for(ancestor of tag.ancestors){
-                                        inheritedATags.set(ancestor.name, ancestor);
-                                    }
-                                }
-                                if([...inheritedATags.values()].length > 0 ){
-                                    let iATagTable = createTable(['Inherited Art Tags'], [...inheritedATags.values()], buildTagTableCell('atag'))
-                                    tagDiv.appendChild(iATagTable)
-                                }
-                                
-                                // Move print info and our tag info into the container we made
-                                cardProfileElement.appendChild(tagDiv)
+            let divider = document.createElement('div');
+            divider.style.height = '0';
+            divider.style.flexBasis = '100%';
+            tagDiv.appendChild(divider);
 
-                                // Fix widths
-                                tagDiv.style.width = '100%'
-                                tagDiv.style.margin = '30px auto'
-                                tagDiv.style.flexFlow = 'row wrap'
-                                tagDiv.style.display = 'flex'
-                                //tagDiv.style.maxWidth = '1000px'
-                                tagDiv.id = 'tag-container'
-                                tagDiv.style.marginTop = '30px'
-                                tagDiv.style.justifyContent= "center";
-                                tagDiv.style.borderTop = '1px dashed #CDCDCD'
-                            })
-                    })
-                })
-                
-            })
+            // Inherited oracle tags table
+            let inheritedOTags = new Map()
+            for(let tag of tags.filter((t) => t.type === 'ORACLE_CARD_TAG')){
+                for(ancestor of tag.ancestors){
+                    inheritedOTags.set(ancestor.name, ancestor);
+                }
+            }
+            if([...inheritedOTags.values()].length > 0){
+                let iOTagTable = createTable(['Inherited Oracle Tags'], [...inheritedOTags.values()], buildTagTableCell('otag'))
+                tagDiv.appendChild(iOTagTable)
+            }
+            
+
+            // Inherited art tags table
+            let inheritedATags = new Map()
+            for(let tag of tags.filter((t) => t.type === 'ILLUSTRATION_TAG')){
+                for(ancestor of tag.ancestors){
+                    inheritedATags.set(ancestor.name, ancestor);
+                }
+            }
+            if([...inheritedATags.values()].length > 0 ){
+                let iATagTable = createTable(['Inherited Art Tags'], [...inheritedATags.values()], buildTagTableCell('atag'))
+                tagDiv.appendChild(iATagTable)
+            }
+            
+            // Move print info and our tag info into the container we made
+            cardProfileElement.appendChild(tagDiv)
+
+            // Fix widths
+            tagDiv.style.width = '100%'
+            tagDiv.style.margin = '30px auto'
+            tagDiv.style.flexFlow = 'row wrap'
+            tagDiv.style.display = 'flex'
+            //tagDiv.style.maxWidth = '1000px'
+            tagDiv.id = 'tag-container'
+            tagDiv.style.marginTop = '30px'
+            tagDiv.style.justifyContent= "center";
+            tagDiv.style.borderTop = '1px dashed #CDCDCD'
             
         }
     }
+}
+
+// Check if this has already run
+if(!document.getElementById('tag-container')){
+    addTags()
 }
